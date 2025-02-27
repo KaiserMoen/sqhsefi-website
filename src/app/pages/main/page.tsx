a4d81c36f0368673f26464a3aaf520e47689a650
Nhelg Kaiser Moen Napeek <nnhelg@gmail.com>
2025-02-27 21:41:07 +0800

    adding school affiliates

--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import Image from "next/image";
-import lofow from "../../../../public/images/logow.png" ;
-import philo from "../../../../public/images/philosophy.webp"
-import Video from "./video"
+import Seameo from "../../../../public/images/seameo.png" ;
+import Apscu from "../../../../public/images/apscu.png";
+import Peac from "../../../../public/images/peac.jpg" ;
+import Video from "./video";
 export default function Main() {
   return (
     <div>
       <div>
         <Video/>
       </div>
-      <div className="flex  flex-col justify-center items-center rounded border-6 border-[#02327a] p-20 gap-12 sm:flex-row">
-        <h1 className="font-bold text-4xl  border-r-4 border-[#EDCD1F]  p-4 ">ABOUT US</h1>
+      <div className="flex  flex-col justify-center items-center rounded border-6 border-[#02327a] p-20 pb-0 gap-12 sm:flex-row">
+        <h1 className="font-bold text-4xl  border-r-4 border-[#EDCD1F]  p-4  ">ABOUT US</h1>
         <div className="md:text-lg sm: flex flex-col justify-center items-center ">
           <div>
           Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
@@ -18,7 +19,36 @@ export default function Main() {
         </div>
       </div>
 
-
+      <div className="flex justify-evenly items-center gap-10 p-5">
+        <div className="relative ">
+          <Image
+              src={Peac}
+              alt="Background Image"
+              quality={100}
+              width={400}
+              height={150} 
+          ></Image>
+        </div>
+        <div className="relative">
+          <Image
+              src={Apscu}
+              alt="Background Image"
+              quality={100}
+              width={200}
+              height={150} 
+          ></Image>
+        </div>
+        <div className="relative">
+          <Image
+              src={Seameo}
+              alt="Background Image"
+              quality={100}
+              width={500}
+              height={150}  
+          ></Image>
+        </div>
+        
+      </div>
       <div className="bg-[#EDCD1F] gap-4 grid grid-cols-2 lg:flex lg:justify-center lg:items-center">
           <div className="flex justify-evenly items-center p-2 text-white font-bold lg:flex-col ">
             <div className="text-[70%] border-[#02327a] p-2 border-b-0 border-r-4 w-[70%] md:text-xl lg:border-b-4 lg:border-r-0 lg:w-[100%]">DEPARTMENTS </div>
