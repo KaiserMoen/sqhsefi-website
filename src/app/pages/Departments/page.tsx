b82286f6a0ed50b6ee3bd77bc35a66a57fa928cb
Nhelg Kaiser Moen Napeek <nnhelg@gmail.com>
2025-03-01 00:44:17 +0800

    Fixed layout

--- src/app/pages/Departments/page.tsx
+++ src/app/pages/Departments/page.tsx
@@ -5,7 +5,8 @@ export default function Departments() {
   return ( 
     <div>
 
-    <div className="flex justify-center items-center h-[100%] bg-gray-100">
+    <div className="flex justify-center items-center flex-col h-[100%]">
+      <div>WHAT WE OFFER</div>
       <div className="w-[100%] h-[100%]">
         <Slider />
       </div>
--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Seameo from "../../../../public/images/seameo.png" ;
 import Apscu from "../../../../public/images/apscu.png";
 import Peac from "../../../../public/images/peac.jpg" ;
+import Prof from "../../../../public/images/professor.webp";
 import Video from "./video";
 export default function Main() {
   return (
@@ -10,40 +11,100 @@ export default function Main() {
       <div>
         <Video/>
       </div>
-      <div className="flex  flex-col justify-center items-center rounded border-6 border-[#02327a] p-20 gap-12 sm:flex-row">
-        <h1 className="font-bold text-6xl  border-r-4 border-[#EDCD1F]  p-4  ">ABOUT US</h1>
-        <div className="md:text-xl sm: flex flex-col justify-center items-center ">
+      <div className="flex  flex-col justify-center items-center rounded border-6 border-[rgb(2,50,122)] p-20 gap-12 sm:flex-row">
+        <h1 className="font-bold text-4xl  border-r-4 border-[#EDCD1F]  p-4 lg:text-6xl">ABOUT US</h1>
+        <div className="md:text-xl sm: flex flex-col ">
           <div>
-          Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
+          <b>Established in 1945</b>, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
+          </div>
+          <div className="gap-4 grid grid-cols-2 lg:grid lg:grid-cols-4  pt-10">
+              <div className="flex justify-evenly p-2  font-bold  flex-col">
+                <div className="text-lg md:text-4xl">1000+</div>
+                <div className="text-[70%]">DEPARTMENTS </div>
+              </div>
+              <div className="flex justify-evenly p-2  font-bold  flex-col">
+                <div className="text-lg md:text-4xl">10</div>
+                <div className="text-[70%]">YEARS OF EDUCATION</div>
+              </div>
+              <div className="flex justify-evenly p-2  font-bold  flex-col">                
+                <div className="text-lg md:text-4xl">1000</div>
+                <div className="text-[70%]">STUDENTS </div>
+              </div>
+              <div className="flex justify-evenly p-2  font-bold  flex-col">                 
+                <div className="text-lg md:text-4xl">10</div>
+                <div className="text-[70%]">TEACHERS</div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[rgb(220,220,221)] p-2 md:p-10">
+        <div className="flex justify-center items-center flex-col p-2 ">
+          <h1 className="text-4xl flex justify-center items-center p-4 font-bold">LOOK INTO OUR VALUES</h1>
+          <p className="text-center text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa animi rem a ut omnis at dolore illo maxime ea praesentium adipisci distinctio voluptatibus cupiditate numquam, unde neque, ipsa illum?</p>
+        </div>
+        <div className="pt-5 px-2 pb-5 flex  flex-col  lg:grid lg:grid-cols-3 gap-2 ">
+          <div className=" relative h-[10rem]  lg:h-[20rem]">
+            <Image
+              src={Prof}
+              alt="Background Image"
+              quality={100}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="flex flex-col relative p-2  bg-white  rounded w-[100%] h-[100%] xl:h-[20rem]">  
+            <h1 className=" font-bold   p-4 truncate text-2xl md:text-[120%] lg:text-3xl">SCHOOL PHILOSOPHY</h1>
+            <div className="relative z-10 md:text-lg sm: flex flex-col justify-center items-center ">
+              <div className="p-2 flex justify-center items-center ">
+              Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
+              </div>
+            </div>
+          </div>
+          <div className=" relative h-[10rem] lg:h-[20rem]">
+            <Image
+              src={Prof}
+              alt="Background Image"
+              quality={100}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="flex flex-col relative p-2  bg-white  rounded w-[100%] h-[100%] xl:h-[20rem]">  
+            <h1 className=" font-bold   p-4 truncate text-2xl md:text-[120%] lg:text-3xl">SCHOOL VISION</h1>
+            <div className="md:text-lg sm: flex flex-col justify-center items-center ">
+              <div className="p-2 flex justify-center items-center">
+              Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
+            
+              </div>
+            </div>
+          </div>
+          <div className=" relative h-[10rem] lg:h-[20rem]">
+            <Image
+              src={Prof}
+              alt="Background Image"
+              quality={100}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="relative p-2  bg-white rounded w-[100%] h-[100%] xl:h-[20rem]">
+            <h1 className=" font-bold   p-4 truncate text-2xl md:text-[120%] lg:text-3xl">SCHOOL MISSION</h1>
+            <div className=" flex flex-col justify-center items-center ">
+              <div className="p-2 flex justify-center items-center">
+              Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="bg-[#EDCD1F] gap-4 grid grid-cols-2 lg:flex lg:justify-center lg:items-center">
-          <div className="flex justify-evenly items-center p-2 text-white font-bold lg:flex-col ">
-            <div className="text-[70%] border-[#02327a] p-2 border-b-0 border-r-4 w-[70%] md:text-xl lg:border-b-4 lg:border-r-0 lg:w-[100%]">DEPARTMENTS </div>
-            <div className="text-xl p-2 md:text-6xl">1000</div>
-          </div>
-          <div className="flex justify-evenly items-center p-2 text-white font-bold lg:flex-col ">
-            <div className="text-[70%] border-[#02327a] p-2 border-b-0 border-r-4 w-[70%] md:text-xl lg:border-b-4 lg:border-r-0 lg:w-[100%]">YEARS OF EDUCATION</div>
-            <div className="text-xl p-2 md:text-6xl">10</div>
-          </div>
-          <div className="flex justify-evenly items-center p-2 text-white font-bold lg:flex-col ">
-            <div className="text-[70%] border-[#02327a] p-2 border-b-0 border-r-4 w-[70%] md:text-xl lg:border-b-4 lg:border-r-0 lg:w-[100%]">STUDENTS </div>
-            <div className="text-xl p-2 md:text-6xl">1000</div>
-          </div>
-          <div className="flex justify-evenly items-center p-2 text-white font-bold lg:flex-col ">
-            <div className="text-[70%] border-[#02327a] p-2 border-b-0 border-r-4 w-[70%] md:text-xl lg:border-b-4 lg:border-r-0 lg:w-[100%]">TEACHERS</div>
-            <div className="text-xl p-2 md:text-6xl">10</div>
-          </div>
-      </div>
       <div className="flex justify-evenly items-center gap-10 p-5">
         <div className="relative ">
           <Image
               src={Peac}
               alt="Background Image"
               quality={100}
-              width={400}
+              width={300}
               height={150} 
           ></Image>
         </div>
@@ -61,39 +122,12 @@ export default function Main() {
               src={Seameo}
               alt="Background Image"
               quality={100}
-              width={500}
+              width={400}
               height={150}  
           ></Image>
         </div>
-        
       </div>
-      <div className="pt-5 px-10 pb-5 gap-10 flex flex-col justify-between items-center xl:flex-row ">
-        <div className="relative p-2  bg-[#02327a]/90 text-white rounded w-[100%] h-[100%] xl:h-[20rem] xl:w-[33.33%]">  
-          <h1 className="flex justify-center items-center relative z-10 font-bold   border-b-4 border-[#EDCD1F] p-4 truncate text-2xl md:text-4xl ">SCHOOL PHILOSOPHY</h1>
-          <div className="relative z-10 md:text-lg sm: flex flex-col justify-center items-center ">
-            <div className=" h-64 overflow-y-auto p-2 flex justify-center items-center">
-            Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
-            </div>
-          </div>
-        </div>
-        <div className="relative p-2  bg-[#02327a]/90 text-white rounded w-[100%] h-[100%] xl:h-[20rem] xl:w-[33.33%]">
-          <h1 className="flex justify-center items-center relative z-10 font-bold   border-b-4 border-[#EDCD1F] p-4 truncate text-2xl md:text-4xl ">SCHOOL VISION</h1>
-          <div className="md:text-lg sm: flex flex-col justify-center items-center ">
-            <div className="h-64 overflow-y-auto p-2 flex justify-center items-center">
-            Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
-           
-            </div>
-          </div>
-        </div>
-        <div className="relative p-2  bg-[#02327a]/90 text-white rounded w-[100%] h-[100%] xl:h-[20rem] xl:w-[33.33%]">
-          <h1 className="flex justify-center items-center relative z-10 font-bold   border-b-4 border-[#EDCD1F] p-4 truncate text-2xl md:text-4xl ">SCHOOL MISSION</h1>
-          <div className=" flex flex-col justify-center items-center ">
-            <div className="h-64 overflow-y-auto p-2 flex justify-center items-center">
-            Established in 1945, San Quintin High School Educational Foundation, Incorporated has long been a center of academic excellence. This private institution is committed to preparing and training its students to become productive members of local, national and global community.
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </div>
 
   );
